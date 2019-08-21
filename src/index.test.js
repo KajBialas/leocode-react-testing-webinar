09b18aa8ff2996e82c52a46ce13f2b2482caf771
Kaj Białas <kaj.bialas@dev-industry.com>
2019-08-21 22:28:58 +0200

    Init

---
+++ src/index.test.js
@@ -0,0 +1,4 @@
+import { add, subtract } from "./index";
+
+console.log('ADD should correctly add 2 numbers', add(5,5) === 10);
+console.log('SUBTRACT should correctly subtract 2 numbers', subtract(5,5) === 0);
