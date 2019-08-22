7d65d5cd4fda11b1dc438c78a6b4d60101e7eb69
Kaj Białas <kaj.bialas@dev-industry.com>
2019-08-22 10:45:05 +0200

    Enzyme final

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,16 @@ import App from './App';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+// it('renders without crashing', () => {
+//   shallow(<App />);
+// });
+//
+// it('should match snapshot', () => {
+//   const { asFragment } = render(<App/>);
+//   expect(asFragment()).toMatchSnapshot();
+// });
 
 it('should match snapshot', () => {
-  const { asFragment } = render(<App/>);
-  expect(asFragment()).toMatchSnapshot();
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });
